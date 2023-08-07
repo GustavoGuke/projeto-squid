f3db61d84068cec6a2b34926de48537291d06615
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2023-08-07 15:17:12 -0300

    mudando a porta do server

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'http://192.168.3.203',
+    port: 3000
+  }
 })
